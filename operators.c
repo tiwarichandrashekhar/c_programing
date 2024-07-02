0d2d7ed7f0d34edcf6f7cd69309382adf67ef648
tiwarichandrashekhar <155417210+tiwarichandrashekhar@users.noreply.github.com>
2024-07-02 22:48:01 +0530

    Add files via upload

---
+++ operators.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main(){
+	
+	int a=5;
+	int b=6;
+//  	 int c;
+//   c=a+b;
+//   b=c-b;
+//   a=c-b;
+//   printf("recerse of two number with using 3rd varaiable a=%d b=%d",a,b);
+ //--------------------------------------------------------------------------------------------------  
+//   a=a+b;
+//   b=a-b;
+//   a=a-b;
+//   printf("recerse of two number without using 3rd varaiable a=%d b=%d",a,b);
+ //---------------------------------------------------------------------------------------------------  
+ 
+ int c=37;
+ int d=c%10;
+ int e=c/10;
+ d=
+ printf("sum of the digit of the number c=%d ",c+d);
+};
