78e401944f46783878b4d28a20d29182f918a0ff
tiwarichandrashekhar <155417210+tiwarichandrashekhar@users.noreply.github.com>
2024-07-02 22:50:22 +0530

    Add files via upload

---
+++ bitwise.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+//	char s1[10]="tiwari",s2[]="shekhar";
+//	printf("%d, %d\n",sizeof(s1),sizeof(s2));
+//	printf("tiwari add is %u\n","sUSHIL");
+//	printf("%d ,%d",sizeof("KISHOR"+1),sizeof("tiwariii")+1);
+//	printf("%d",sizeof("1.224"));
+//printf("%d",sizeof(printf("kishore")));//,printf("%d",sizeof("tiwariii")));
+//printf("%d,%d\n",sizeof("1.234"),sizeof("1.234"+10));
+
+printf("%d\n",-5%2);
+printf("%d\n",-5%-2);
+printf("%d\n",5%-2);
+printf("%d\n",-5/2);
+printf("%d\n",-5/-2);
+printf("%d\n",5/-2);
+
+printf("%d\n",-5/2);
+printf("%d\n",-5%2);
+printf("%d\n",-5%2);
+
+
+}
