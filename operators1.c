e1c841c953b8f4280085cc665c83ac37b9da93a4
tiwarichandrashekhar <155417210+tiwarichandrashekhar@users.noreply.github.com>
2024-07-02 22:48:33 +0530

    Add files via upload

---
+++ operators1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	int a=4;
+	int c=6;
+	float b=7.5;
+	int d=2;
+	printf("%d\n",a+c);
+	printf("%d\n",a-c);
+	printf("%d\n",a%c);
+	printf("%d\n",a/c);
+	printf("%d\n",a*c);
+	printf("%d\n",c%a);
+	printf("%d\n",c/a);
+	printf("%f\n",b/d);
+	
+	
+}
