71c1c2fce35f1932190520473cb04df020114d4b
chandra shekhar tiwari <155417210+tiwarichandrashekhar@users.noreply.github.com>
2024-07-05 09:56:38 +0530

    Add files via upload

---
+++ e_OR_O_switch.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+    
+    
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+    
+    
+    switch (number % 2) {
+        case 0:
+            printf("%d is even.\n", number);
+            break;
+         case 1:
+         case -1:
+            printf("%d is odd.\n", number);
+            break;
+    }
+    
+    return 0;
+}
